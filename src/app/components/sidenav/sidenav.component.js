e798e566a04fd81c1ad2b1d96800e184c623a54e
nimedev <nicolasmejiaco@gmail.com>
2016-01-18 20:23:54 -0500

    sidenav: close sidenav in mobile viewport when touch a menu-item.

--- src/app/components/sidenav/sidenav.component.js
+++ src/app/components/sidenav/sidenav.component.js
@@ -16,36 +16,51 @@
       templateUrl: 'app/components/sidenav/sidenav.tmpl.html'
     });
 
-  Controller.$inject = ['$window', 'sideNav'];
+  Controller.$inject = ['$location', '$window', 'sideNav'];
 
   /**
    * controller for sidenav component.
    * @name Controller
    * @memberof appSidenav
+   * @param {Object} $location - to redirect.
    * @param {Object} $window - to get intial size.
    * @param {Object} sideNav - to control open/close of sidenav.
    */
-  function Controller($window, sideNav) {
+  function Controller($location, $window, sideNav) {
     var vm = this;
 
     /** controller fields */
     vm.sidenav = sideNav;
 
     /** controller methods */
+    vm.goToLink = goToLink;
     vm.toggle = toggle;
 
     activate();
 
     ////////////
-    /** init function */
+    /** Init function */
     function activate() {
       // check 
-      if($window.innerWidth >= 960) {
+      if ($window.innerWidth >= sideNav.mdBreak) {
         sideNav.closed = false;
       }
     }
     
-    /** toggle sidenav */
+    /**
+     * Go to link
+     * @param {string} path - path to redirect 
+     */
+    function goToLink(path) {
+      if (path) {
+        $location.path(path);
+      }
+      if ($window.innerWidth < sideNav.smBreak) {
+        sideNav.close();
+      }
+    }
+    
+    /** Toggle sidenav */
     function toggle() {
       sideNav.toggle();
     }
--- src/app/components/sidenav/sidenav.service.js
+++ src/app/components/sidenav/sidenav.service.js
@@ -9,23 +9,37 @@
   Service.$inject = [];
   
   /**
-   * function to control sidenav component.
+   * Function to control sidenav component.
    * @name sideNav
    * @memberof sidenav
    */
   function Service() {
     var service = {
       // factory fields
+      smBreak: 600,
+      mdBreak: 960,
       closed: true,
       
       // factory functions
+      close: close,
+      open: open,
       toggle: toggle
     };
 
     return service;
 
     ///////////////
-    /** change sidenav visibility */
+    /** Close sidenav */
+    function close() {
+      service.closed = true;
+    }
+    
+    /** open sidenav */
+    function open() {
+      service.closed = false;
+    }
+    
+    /** Change sidenav visibility */
     function toggle() {
       service.closed = !service.closed;
     }
