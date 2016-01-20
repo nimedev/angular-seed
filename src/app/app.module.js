fd6b3b8214d8d73986c06a50b638637c4eedb88d
nimedev <nicolasmejiaco@gmail.com>
2016-01-20 16:12:15 -0500

    angular: remove html5 friendly.

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -25,19 +25,5 @@
       
     // template module is added with gulp task scritps
       'templates'
-    ])
-    .config(config);
-
-  config.$inject = ['$locationProvider'];
-
-  /**
-   * Settings for angular-seed module.
-   * @name config
-   * @memberof angular-seed
-   * @param {Object} $locationProvider - to friendly html.
-   */
-  function config($locationProvider) {
-    // get rid of the hash in the URL
-    $locationProvider.html5Mode(true);
-  }
+    ]);
 })();
