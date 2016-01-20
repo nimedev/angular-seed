ce3d81c82389f80e7e4582fe42e696203a76009e
nimedev <nicolasmejiaco@gmail.com>
2016-01-19 21:53:33 -0500

    layout: change layout

--- src/app/components/sidenav/sidenav.service.js
+++ src/app/components/sidenav/sidenav.service.js
@@ -32,16 +32,35 @@
     /** Close sidenav */
     function close() {
       service.closed = true;
+      toggleAppContent();
     }
     
-    /** open sidenav */
+    /** Open sidenav */
     function open() {
       service.closed = false;
+      toggleAppContent();
     }
     
     /** Change sidenav visibility */
     function toggle() {
       service.closed = !service.closed;
+      toggleAppContent();
+    }
+    
+    /** HELPER FUNCTIONS */
+    /** toggle class in .app-content element */
+    function toggleAppContent() {
+      var body = angular.element(document).find('body');
+      var element = angular.element(document.querySelector('.app-content'));
+      if (service.closed) {
+        element.removeClass('nav-open');
+        element.addClass('nav-closed');
+        body.removeClass('nav-open');
+      } else {
+        element.removeClass('nav-closed');
+        element.addClass('nav-open');
+        body.addClass('nav-open');
+      }
     }
   }
 })();
