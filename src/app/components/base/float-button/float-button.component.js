6e5d437d9101ceee6a65139dc33674dc1a57bcac
nimedev <nicolasmejiaco@gmail.com>
2016-01-21 18:29:03 -0500

    gulp: improve style and script watchers

---
+++ src/app/components/base/float-button/float-button.component.js
@@ -0,0 +1,14 @@
+/**
+ * FAB button component
+ * @name floatButton
+ * @memberof base
+ */
+(function () {
+  'use strict';
+
+  angular
+    .module('base')
+    .component('floatButton', {
+      templateUrl: 'app/components/base/float-button/float-button.tmpl.html'
+    });
+})();
